39f61f7c39be29c8859e651443e45ca1a2590337
geddadasuresh62348 <lrnothyp@gmail.com>
2023-07-12 15:21:12 +0530

    data modified

--- src/kafka_config/__init__.py
+++ src/kafka_config/__init__.py
@@ -1,17 +1,21 @@
 
 import os
 
-
+# Security related variables
 SECURITY_PROTOCOL="SASL_SSL"
 SSL_MACHENISM="PLAIN"
+
+#  Cloud related variables
 API_KEY = os.getenv('API_KEY',None)
-ENDPOINT_SCHEMA_URL  = os.getenv('ENDPOINT_SCHEMA_URL',None)
 API_SECRET_KEY = os.getenv('API_SECRET_KEY',None)
 BOOTSTRAP_SERVER = os.getenv('BOOTSTRAP_SERVER',None)
 # SECURITY_PROTOCOL = os.getenv('SECURITY_PROTOCOL',None)
 # SSL_MACHENISM = os.getenv('SSL_MACHENISM',None)
+
+#  Schema related variables
 SCHEMA_REGISTRY_API_KEY = os.getenv('SCHEMA_REGISTRY_API_KEY',None)
 SCHEMA_REGISTRY_API_SECRET = os.getenv('SCHEMA_REGISTRY_API_SECRET',None)
+ENDPOINT_SCHEMA_URL  = os.getenv('ENDPOINT_SCHEMA_URL',None)
 
 
 def sasl_conf():
